90a544ae2fd41ef1a3a6167962d5cb877fa38093
Jianying Chiang <39771683+GrandpaBear@users.noreply.github.com>
2019-12-01 19:31:09 -0500

    main

--- criblife/src/components/main/main.jsx
+++ criblife/src/components/main/main.jsx
@@ -1,6 +1,11 @@
 import React from "react";
 import axios from "axios";
 import "bootstrap/dist/css/bootstrap.min.css";
+import "./main.scss";
+
+import UnfoldMoreNonActive from "../../images/unfoldmorenonactive.png";
+import UnfoldMoreActive from "../../images/unfoldmoreactive.png";
+import UnfoldLess from "../../images/unfoldless.png";
 
 export class Main extends React.Component {
   constructor(props) {
@@ -11,15 +16,46 @@ export class Main extends React.Component {
   render() {
     return (
       <div className="row">
-        <div className="col-lg-8">
+        <div className="col-lg-7">
           <div className="row">
-            <div className="col-lg-6">
-              Filters: Term, Length, Price, Rooms, Whole place,{" "}
+            <div className="col-lg-12">
+              <div className="main-filter">
+                <div className="main-filter-dropdown">
+                  <button type="button" className="main-filter-button">
+                    Any Term
+                  </button>
+                </div>
+                <div className="main-filter-dropdown">
+                  <div className="main-filter-button">
+                    Any Length
+                    <img src={UnfoldMoreNonActive} />
+                  </div>
+                </div>
+                <div className="main-filter-dropdown">
+                  <button type="button" className="main-filter-button">
+                    $9999-$9999
+                  </button>
+                </div>
+                <div className="main-filter-dropdown">
+                  <button type="button" className="main-filter-button">
+                    Any # Rooms
+                  </button>
+                </div>
+                <div className="main-filter-dropdown">
+                  <button type="button" className="main-filter-button">
+                    Whole place
+                  </button>
+                </div>
+                <div className="main-filter-dropdown">
+                  <button type="button" className="main-filter-button">
+                    More Filters
+                  </button>
+                </div>
+              </div>
             </div>
-            <div className="col-lg-6">MoreFilters</div>
           </div>
         </div>
-        <div className="col-lg-4">Map</div>
+        <div className="col-lg-5">Map</div>
       </div>
     );
   }
